0b10c57f59ee1e723ff58cefb599729cbdcff858
estyhairina <1910131320007@mhs.ulm.ac.id>
2022-09-23 09:44:16 +0700

    update

--- project_perangkat_lunak/js/myscript.js
+++ project_perangkat_lunak/js/myscript.js
@@ -3,12 +3,14 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.2/firebase
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-import { getAuth, createUserWithEmailAndPassword,
-signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-auth.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-auth.js";
+
+import { getDatabase, set, ref, update } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-auth.js";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBPg2EcBQL-NMY0XE4PlJTQVTYI_IS2e2I",
   authDomain: "project-media-pembelajaran.firebaseapp.com",
+  databaseURL: "https://project-media-pembelajaran-default-rtdb.firebaseio.com",
   projectId: "project-media-pembelajaran",
   storageBucket: "project-media-pembelajaran.appspot.com",
   messagingSenderId: "992474772188",
@@ -17,3 +19,61 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const database = getDatabase(app);
+
+const signInbtn = document.getElementById("sign-in-btn");
+const signUpbtn = document.getElementById("sign-up-btn");
+
+signUpbtn.addEventListener('click', (e) =>{
+  // let name = document.getElementById("name").value;
+  // let Email = document.getElementById("email-sign-up").value;
+  // let pwSignUp = document.getElementById("pw-sign-up").value;
+  // let pwConfir = document.getElementById("pw-confirm").value;
+
+  var name = document.getElementById("name").value;
+  var Email = document.getElementById("email-sign-up").value;
+  var pwSignUp = document.getElementById("pw-sign-up").value;
+  
+  createUserWithEmailAndPassword(auth, Email, pwSignUp)
+  .then((userCredential) => {
+    // Signed in
+    const user = userCredential.user;
+
+    // set(ref(database, "users/" + user.uid), {
+    //   name : name,
+    //   Email : Email,
+    //   pwSignUp : pwSignUp
+    //   });
+    // ...
+    alert("User Telah ditambahkan");
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+    alert(errorMessage);
+  });
+
+
+  // createUserWithEmailAndPassword(auth, Email, pwSignUp)
+  //   .then((userCredential) => {
+  //     // Signed in
+  //     const user = userCredential.user;
+  //     alert("User Telah ditambahkan");
+
+  //     set(ref(database, "users/" + user.uid)), {
+  //       name : name,
+  //       Email : Email,
+  //       pwSignUp : pwSignUp
+
+  //     });
+  //   })
+
+  //   .catch((error) => {
+  //     const errorCode = error.code;
+  //     const errorMessage = error.message;
+
+  //     alert(errorMessage);
+  //   });
+});
