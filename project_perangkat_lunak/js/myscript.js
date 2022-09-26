6036478adb8fac3de49f2530ff5ba3d37c0a97b1
estyhairina <1910131320007@mhs.ulm.ac.id>
2022-09-26 09:04:12 +0700

    Update

--- project_perangkat_lunak/js/myscript.js
+++ project_perangkat_lunak/js/myscript.js
@@ -5,7 +5,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.2/firebase
 
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-auth.js";
 
-import { getDatabase, set, ref, update } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-auth.js";
+import { getDatabase, set, ref, update } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-database.js";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBPg2EcBQL-NMY0XE4PlJTQVTYI_IS2e2I",
@@ -26,25 +26,25 @@ const signInbtn = document.getElementById("sign-in-btn");
 const signUpbtn = document.getElementById("sign-up-btn");
 
 signUpbtn.addEventListener('click', (e) =>{
-  // let name = document.getElementById("name").value;
-  // let Email = document.getElementById("email-sign-up").value;
-  // let pwSignUp = document.getElementById("pw-sign-up").value;
-  // let pwConfir = document.getElementById("pw-confirm").value;
+  let name = document.getElementById("name").value;
+  let Email = document.getElementById("email-sign-up").value;
+  let pwSignUp = document.getElementById("pw-sign-up").value;
+  let pwConfir = document.getElementById("pw-confirm").value;
 
-  var name = document.getElementById("name").value;
-  var Email = document.getElementById("email-sign-up").value;
-  var pwSignUp = document.getElementById("pw-sign-up").value;
+  // var name = document.getElementById("name").value;
+  // var Email = document.getElementById("email-sign-up").value;
+  // var pwSignUp = document.getElementById("pw-sign-up").value;
   
   createUserWithEmailAndPassword(auth, Email, pwSignUp)
   .then((userCredential) => {
     // Signed in
     const user = userCredential.user;
 
-    // set(ref(database, "users/" + user.uid), {
-    //   name : name,
-    //   Email : Email,
-    //   pwSignUp : pwSignUp
-    //   });
+    set(ref(database, "users/" + user.uid), {
+      name : name,
+      Email : Email,
+      pwSignUp : pwSignUp
+      });
     // ...
     alert("User Telah ditambahkan");
   })
