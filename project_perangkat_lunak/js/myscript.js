b44f0bdad64d3bb55130cd8a24591806b84c2d70
estyhairina <1910131320007@mhs.ulm.ac.id>
2022-10-02 21:36:21 +0700

    Update Esty

--- project_perangkat_lunak/js/myscript.js
+++ project_perangkat_lunak/js/myscript.js
@@ -1,11 +1,9 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-database.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-auth.js";
-
-import { getDatabase, set, ref, update } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-database.js";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBPg2EcBQL-NMY0XE4PlJTQVTYI_IS2e2I",
@@ -19,39 +17,24 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
 const database = getDatabase(app);
+const auth = getAuth();
 
-const signInbtn = document.getElementById("sign-in-btn");
-const signUpbtn = document.getElementById("sign-up-btn");
+signUp.addEventListener('click', (e) => {
+  var Email = document.getElementById('emailSignUp').value;
+  var Password = document.getElementById('pwSignUp').value;
 
-signUpbtn.addEventListener('click', (e) =>{
-  let name = document.getElementById("name").value;
-  let Email = document.getElementById("email-sign-up").value;
-  let pwSignUp = document.getElementById("pw-sign-up").value;
-  let pwConfir = document.getElementById("pw-confirm").value;
-
-  // var name = document.getElementById("name").value;
-  // var Email = document.getElementById("email-sign-up").value;
-  // var pwSignUp = document.getElementById("pw-sign-up").value;
-
-  createUserWithEmailAndPassword(auth, Email, pwConfir)
+  createUserWithEmailAndPassword(auth, Email, Password)
   .then((userCredential) => {
-    // Signed in
-    const user = userCredential.user;
-
-    set(ref(database, "users/" + user.uid), {
-      name : name,
-      Email : Email,
-      pwSignUp : pwSignUp
-      });
-    // ...
-    alert("User Telah ditambahkan");
+  // Signed in
+  const user = userCredential.user;
+  alert("Berhasil");
+  // ...
   })
   .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // ..
-    alert(errorMessage);
+  const errorCode = error.code;
+  const errorMessage = error.message;
+  alert(errorMessage);
+  // ..
   });
 });
