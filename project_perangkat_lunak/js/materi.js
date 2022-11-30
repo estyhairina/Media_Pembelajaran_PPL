49217ecd96a1e3bac988f8ad5c7f53c96404f7cc
estyhairina <1910131320007@mhs.ulm.ac.id>
2022-11-30 19:26:06 +0700

    update esty cantik

--- project_perangkat_lunak/js/materi.js
+++ project_perangkat_lunak/js/materi.js
@@ -16,3 +16,41 @@ function ftampil(){
   		});
 	}
 }
+
+window.onscroll = function(){
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+	  document.getElementById("js-top").style.display = "block";
+	} else{
+	  document.getElementById("js-top").style.display = "none";
+	}
+  };
+
+function tujuan(tujuan) {
+    var x = document.getElementById(tujuan);
+  
+    if (x.className.indexOf("hilang") == -1) {
+      x.className += " hilang";
+    } else {
+      x.className = x.className.replace("hilang", "");
+    }
+}
+
+function apersepsi(apersepsi) {
+    var x = document.getElementById(apersepsi);
+  
+    if (x.className.indexOf("lost") == -1) {
+      x.className += " lost";
+    } else {
+      x.className = x.className.replace("lost", "");
+    }
+  }
+
+  function tts(tts) {
+    var x = document.getElementById(tts);
+  
+    if (x.className.indexOf("lost") == -1) {
+      x.className += " lost";
+    } else {
+      x.className = x.className.replace("lost", "");
+    }
+  }
