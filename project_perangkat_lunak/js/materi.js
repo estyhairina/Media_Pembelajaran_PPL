f658fb2a0c4713d0d73ac183eb9cf2c5e4a595e3
estyhairina <1910131320007@mhs.ulm.ac.id>
2022-12-13 22:38:40 +0700

    update

--- project_perangkat_lunak/js/materi.js
+++ project_perangkat_lunak/js/materi.js
@@ -1,3 +1,16 @@
+function dafis(){
+  let dafis = document.getElementById("dafis");
+  let main = document.getElementById("main");
+
+  if (dafis.className.indexOf("dafis-hilang") == -1){
+    dafis.className += "dafis-hilang";
+    main.className = main.className.replace("main","main-penuh");
+  } else{
+    dafis.className = dafis.className.replace("dafis-hilang","");
+    main.className = main.className.replace("main-penuh","main");
+  }
+}
+
 function ftampil(){
 	/* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
 	var dropdown = document.getElementsByClassName("dropdown-btn");
